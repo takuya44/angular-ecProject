7f51b8948b66008805e73b003b7dad11e3ea5094
takuya44 <shinntaku2345@gmail.com>
2024-07-24 19:41:47 +0900

    ✨ 商品リスト表示① カテゴリ表示（*ngForディレクティブ, trackBy）

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  // 家電製品のカテゴリリスト:publicにしておくことで、テンプレートからアクセスできる
+  public categoryList: string[] = [
+    'Fridge',
+    'Washer',
+    'Kitchen',
+    'Vacuum',
+    'Climate',
+    'TV',
+    'Other',
+  ];
 
+  // カテゴリリストのインデックスを返す：不要なDOM操作が減り、アプリケーションのパフォーマンスが向上
+  public trackByIndex(index: number, item: any): number {
+    return index;
+  }
 }
