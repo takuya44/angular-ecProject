54df49d962a66fc173f752555f87deaf272defd7
takuya44 <shinntaku2345@gmail.com>
2024-07-24 19:10:33 +0900

    ✨ ホーム画面追加＆リダイレクト（Route設定, router-outletディレク)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'; // Angularのルーティング機能をインポート
+import { HomeComponent } from './components/home/home.component'; // HomeComponentをインポート
 
-export const routes: Routes = [];
+// アプリケーションのルート定義
+export const routes: Routes = [
+  {
+    path: '', // ルートパス（http://localhost:4200）
+    redirectTo: 'home', // 'home'パスにリダイレクト
+    pathMatch: 'full', // 完全一致が必要
+  },
+  {
+    path: 'home', // 'home'パス（http://localhost:4200/home）
+    component: HomeComponent, // HomeComponentを表示
+  },
+];
