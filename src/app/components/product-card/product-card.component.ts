a823b71472631f70022115d0e641bb2df71c8ea1
takuya44 <shinntaku2345@gmail.com>
2024-07-25 22:43:22 +0900

    🚑 ✨ 商品リスト表示⑤ カート追加・在庫切れボタン表示（*ngIfディレクティブ）高さ調整

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
